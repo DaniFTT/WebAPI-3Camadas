6fdb227d368b009c845f1adc623ec9417b1e48eb
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-10 21:15:52 -0300

    Metodos do serviço de produto declarados e implementados

---
+++ BLL/Interfaces/Services/IProdutoService.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces.Services
+{
+    public interface IProdutoService
+    {
+        Task<List<ProdutoModel>> ListarProdutosMaisComprados();
+        Task<List<ProdutoModel>> ListarProdutosMaisRecentes();
+        Task<List<ProdutoModel>> ListarProdutosInativos();
+        Task<List<ProdutoModel>> ListarProdutosAtivos();
+    }
+}
---
+++ BLL/Services/ProdutoService.cs
@@ -0,0 +1,47 @@
+﻿using Business.Interfaces;
+using Business.Interfaces.Services;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class ProdutoService : IProdutoService
+    {
+        private readonly IProduto _produto;
+
+        public ProdutoService(IProduto produto)
+        {
+            _produto = produto;
+        }
+
+        public async Task<List<ProdutoModel>> ListarProdutosMaisComprados()
+        {
+            //var list = await ListarProdutos(p => p.Ativo);
+            //return list.OrderBy(p => p.DataDeCriacao).Take(5).ToList();
+            var list = await _produto.ListarProdutos(whereExpProd: prod => prod.Ativo, orderByDescendingExpProd: prod => prod.QuantidadeVendida);
+            return list;
+        }
+
+        public async Task<List<ProdutoModel>> ListarProdutosMaisRecentes()
+        {
+            //var list = await ListarProdutos(whereExpProd: w => w.Ativo);
+            //return list.OrderBy(p => p.DataDeCriacao).Take(5).ToList();
+            var list = await _produto.ListarProdutos(whereExpProd: prod => prod.Ativo, orderByExpProd: prod => prod.DataDeCriacao);
+            return list;
+        }
+
+        public async Task<List<ProdutoModel>> ListarProdutosInativos()
+        {
+            return await _produto.ListarProdutos(whereExpProd: prod => !prod.Ativo);
+        }
+
+        public async Task<List<ProdutoModel>> ListarProdutosAtivos()
+        {
+            return await _produto.ListarProdutos(whereExpProd: prod => prod.Ativo);
+        }
+    }
+}
