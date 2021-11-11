3988553b0ec92cf1f83b38658a6039d56ef36acd
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-10 21:15:29 -0300

    Metodo de listar produtos implementado no repository

---
+++ DAL/Repository/ProdutoRepository.cs
@@ -0,0 +1,36 @@
+﻿using Business.Interfaces;
+using Data.Config;
+using Data.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class ProdutoRepository : GenericRepository<ProdutoModel>, IProduto
+    {
+        private readonly DbContextOptions<ContextBase> _optionsBuilder;
+        public ProdutoRepository()
+        {
+            _optionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task<List<ProdutoModel>> ListarProdutos(Expression<Func<ProdutoModel, bool>> whereExpProd = null, 
+                                                             Expression<Func<ProdutoModel, object>> orderByExpProd = null, 
+                                                             Expression<Func<ProdutoModel, object>> orderByDescendingExpProd = null)
+        {
+            using (var data = new ContextBase(_optionsBuilder))
+            {
+                return await data.ProdutoModel
+                    .Where(whereExpProd)
+                    .OrderBy(orderByExpProd)
+                    .ThenByDescending(orderByDescendingExpProd).AsNoTracking().ToListAsync();
+            }
+        }
+    }
+}
