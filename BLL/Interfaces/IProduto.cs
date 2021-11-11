6a87e0692fc977a86030e87181cdaf76ae7464b0
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-11-10 21:11:47 -0300

    Criado metodo utilizando de expresions

--- BLL/Interfaces/IProduto.cs
+++ BLL/Interfaces/IProduto.cs
@@ -3,6 +3,7 @@ using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,8 @@ namespace Business.Interfaces
 {
     public interface IProduto : IGeneric<ProdutoModel>
     {
+        Task<List<ProdutoModel>> ListarProdutos(Expression<Func<ProdutoModel, bool>> whereExpProd = null,
+                                                Expression<Func<ProdutoModel, object>> orderByExpProd = null,
+                                                Expression<Func<ProdutoModel, object>> orderByDescendingExpProd = null);
     }
 }
